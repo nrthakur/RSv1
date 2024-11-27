606b456b4a6d0006ea545d98f5355fc6c1e11097
Nikunj Thakur <nikunj.thakur@thescore.com>
2024-11-27 13:58:31 -0500

    (auth): Signout Functionality + Navigation Bar & Transistions.

---
+++ RSv1/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  RSv1
+//
+//  Created by Nikunj Thakur on 2024-11-27.
+//
+
+import Foundation
---
+++ RSv1/ListingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingsView.swift
+//  RSv1
+//
+//  Created by Nikunj Thakur on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ListingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingsView()
+}
